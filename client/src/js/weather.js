822bf5a9b8ac5d5ded627d0f3a9665db142cf949
Yash <raunakyash12@gmail.com>
2026-03-14 01:45:59 +0530

    fixed bug

--- client/src/js/weather.js
+++ client/src/js/weather.js
@@ -1,91 +1,107 @@
 import getWeather from "./fetchWeatherInfo.js";
-import { graphTemp,graphAll} from "./allChart.js";
-import loadComponent from "./loadcomponents.js"
-import addHamburger from "./hamburger.js"
+import { graphTemp, graphAll } from "./allchart.js";
+import loadComponent from "./loadcomponents.js";
+import addHamburger from "./hamburger.js";
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header2", "header", () => addHamburger());
+  loadComponent("sidebar", "sidebar");
 
-    loadComponent("header2","header",()=>addHamburger());
-    loadComponent("sidebar","sidebar");
+  gsap.registerPlugin(Flip, ScrollTrigger, ScrollToPlugin);
 
-    gsap.registerPlugin(Flip,ScrollTrigger,ScrollToPlugin);
+  function preLoader() {
+    document.querySelector(".pl-logo").innerHTML = "Weather";
+    const tl = gsap.timeline();
 
-    function preLoader() {
-        document.querySelector(".pl-logo").innerHTML = "Weather";
-        const  tl = gsap.timeline();
+    tl.to(".overlay", {
+      xPercent: 100,
+      delay: 1,
+      duration: 0.5,
+    })
+      .fromTo(
+        ".page-header",
+        {
+          y: -100,
+        },
+        {
+          y: 0,
+        },
+      )
+      .fromTo(
+        ".searchBar",
+        {
+          y: -10,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+        },
+      )
+      .fromTo(
+        "#chart-section",
+        {
+          x: 200,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          opacity: 1,
+        },
+      )
+      .fromTo(
+        ".city",
+        {
+          y: -100,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+        },
+      );
+    gsap.fromTo(
+      ".temp-graph-container",
+      {
+        x: -200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+      },
+    );
+  }
 
-        tl.to(".overlay", {
-            xPercent: 100,
-            delay:1,
-            duration: 0.5,
-        }).fromTo(".page-header",{
-            y:-100
-        },{
-            y:0
-        })
-        .fromTo(".searchBar",{
-            y:-10,
-            opacity:0,
-            
-        },{
-            y:0,
-            opacity:1,
-            duration: 1, 
-        })
-        .fromTo("#chart-section",{
-            x:200,
-            opacity:0,
-        },{
-            x:0,
-            opacity:1,
-        })
-        .fromTo(".city",{
-            y:-100,
-            opacity:0,
-        },{
-            y:0,
-            opacity:1,
-        })
-        gsap.fromTo(".temp-graph-container",
-            {
-            x:-200,
-            opacity:0,
-        },{
-            x:0,
-            opacity:1,
-        })
-    } 
+  const gauge = document.querySelector("#gauge");
+  gauge.setAttribute("data-value", "10");
 
+  async function addMap() {
+    const data = await getWeather();
+    const lat = data.main.coord.lat;
+    const lon = data.main.coord.lon;
 
+    const map = L.map("map").setView([lat, lon], 13);
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+      {
+        subdomains: "abcd",
+        maxZoom: 19,
+      },
+    ).addTo(map);
+    let marker = L.marker([lat, lon]).addTo(map);
+    marker.bindPopup(`<b>${data.city}</b>`).openPopup();
+  }
 
-    const gauge = document.querySelector("#gauge");
-    gauge.setAttribute("data-value","10")
-
-    async function addMap() {
-        const data =  await getWeather();
-        const lat = data.main.coord.lat;
-        const lon = data.main.coord.lon;
-        
-        const map = L.map('map').setView([lat, lon], 13);
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-            subdomains: 'abcd',
-            maxZoom: 19,
-          }).addTo(map);
-        let marker = L.marker([lat, lon]).addTo(map);
-        marker.bindPopup(`<b>${data.city}</b>`).openPopup();
+  async function graph() {
+    const state = await graphTemp(document.getElementById("tempGraph"));
+    if (state) {
+      graphAll(document.getElementById("myChart"));
     }
+  }
 
-    async function graph(){
-        const state = await graphTemp(document.getElementById('tempGraph'));
-        if(state){
-            graphAll(document.getElementById('myChart'));
-        }    
-
-    }
-
-    preLoader();
-    addMap();
-    graph();
-    
+  preLoader();
+  addMap();
+  graph();
 });
-
