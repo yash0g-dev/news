16b339bfe7b3a43f9c1238d5fd249b3d4a841d87
Yash <raunakyash12@gmail.com>
2026-03-13 18:03:52 +0530

    fix: apply gitignore and remove tracked files

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import javascriptLogo from "./assets/javascript.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import { setupCounter } from "./counter.js";
-
-document.querySelector("#app").innerHTML = `
-
-`;
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-export default {
-    server: {
-      proxy: {
-        '/scrape': 'http://localhost:3000', // Proxy requests to your Express server
-      },
-    },
-  };
-  
